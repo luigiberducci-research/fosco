1ec5f1b4e106a242a1f4d276d4eda25681962e55
Luigi Berducci <luigi.berd@gmail.com>
2024-01-24 11:46:23 +0100

    return dict of models

--- barriers/__init__.py
+++ barriers/__init__.py
@@ -1,9 +1,14 @@
 from models.torchsym import TorchSymModel
+from systems import ControlAffineDynamics
 
 
-def make_barrier(id: str, **kwargs) -> TorchSymModel:
-    if id == "single_integrator":
+def make_barrier(system: ControlAffineDynamics, **kwargs) -> dict[str, TorchSymModel]:
+    if system.id == "single_integrator":
         from barriers.single_integrator import SingleIntegratorCBF
-        return SingleIntegratorCBF(**kwargs)
+        return {
+            "barrier": SingleIntegratorCBF(system=system),
+            "compensator": None,
+        }
+
     else:
         raise NotImplementedError(f"barrier {id} not implemented")
